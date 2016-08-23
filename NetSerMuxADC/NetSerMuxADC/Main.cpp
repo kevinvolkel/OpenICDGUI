768b93b51a0c1066f5aa6dc49b72d5cc090c02d9
kevinvolkel <kevin.volkel@wilkes.edu>
2016-08-23 11:32:01 -0400

    Add files via upload

---
+++ NetSerMuxADC/NetSerMuxADC/Main.cpp
@@ -0,0 +1,168 @@
+#include<io.h>
+#include<stdio.h>
+#include<winsock2.h>
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <math.h>
+#include <sstream>
+#include <fstream>
+#include <windows.h>
+#include <conio.h>
+#include "SendFilestruct.h"
+#include"Serial_Port_functions.h"
+#include"Winsock_Functions.h"
+
+
+
+#pragma comment(lib,"ws2_32.lib") //Winsock Library
+
+using namespace std;
+
+
+
+
+
+
+// buffers for data from file
+
+double buffer_data_in_N[30000];
+double buffer_data_in_X[30000];
+double buffer_data_in_Y[30000];
+double buffer_data_in_Z[30000];
+double buffer_data_OUT[150000];
+
+
+//data buffers for sending data
+char sendAStop[30000];
+char *pointAStop = sendAStop;
+char sendASbot[30000];
+char *pointASbot = sendASbot;
+char sendVStop[30000];
+char *pointVStop = sendVStop;
+char sendVSbot[30000];
+char *pointVSbot = sendVSbot;
+char sendSStop[30000];
+char *pointSStop = sendSStop;
+char sendSSbot[30000];
+char *pointSSbot = sendSSbot;
+char rec = 0;
+int s = 0;
+int count = 0;
+// serial handle and variabels
+HANDLE serialh;
+// Set params
+DCB serialparams = { 0 };
+int error;
+DWORD x, y;
+
+//variables and structures for client server communication
+char abortflag = 0;
+char *message;
+char incoming[200000];
+int receivesize;
+
+WSADATA wsa;
+SOCKET sock, new_socket;
+struct sockaddr_in server, client;
+
+//variables for which device is being tested and what number file the loop is on
+int deviceid;
+int filenum;
+
+
+
+//variables for socket sending
+struct Filestruct struct4_send;
+
+
+
+
+
+
+int main(int argc, char* argv[]) {
+	//extracting parameters from the argument array
+	string deviceidstring(argv[1]);
+	string filenumber(argv[2]);
+	string comnum(argv[3]);
+	deviceid = stoi(deviceidstring);
+	filenum = stoi(filenumber);
+	//open output file for the ADC data
+	ofstream ADC("ADCdata.txt");
+	//LSB variable
+	double LSB = 3.723e-5;
+
+	message = "ok";
+	// call to set up serial
+	setserial(&serialh, &serialparams,comnum);
+	CloseHandle(serialh);
+	setserial(&serialh, &serialparams,comnum);
+
+
+	//initiate server
+	abortflag = initserver(&wsa, &sock, &new_socket, &server, &client);
+	if (abortflag == 1) return 0;
+
+
+
+
+
+	while (1) {
+		cout << "Waiting for start" << endl << endl;
+		//recieve initiation of data
+		receivesize = recv(new_socket, incoming, 1000, 0);
+		incoming[receivesize] = '\0';
+		string init(incoming);
+		//if start is received first then a new data file is coming, if 'done' is received then shut server down
+		if (init == "start") {
+			//send a confirmation message (arbitrary message
+			send(new_socket, message, strlen(message), 0);
+			//receive the name of the text file that is sent
+			receivesize = recv(new_socket, incoming, 1000, 0);
+			incoming[receivesize] = '\0';
+			string textfilename(incoming);
+			cout << "Begin collecting Data" << endl << endl;
+
+			//function for receiving EGM file from PC1
+			rec_EGM_txt(&new_socket, incoming, buffer_data_in_N, buffer_data_in_X, buffer_data_in_Y, buffer_data_in_Z);
+
+			cout << "data collected" << endl << endl;
+
+			//funciton call that calculates values for the DAC
+			convert2_DAC(sendAStop, sendASbot, sendVStop, sendVSbot, sendSStop, sendSSbot, buffer_data_in_X, buffer_data_in_Y, buffer_data_in_Z, LSB);
+
+			//function that sends EGM data to either the ICD or the algorithm, also gets ICD shock data from the microcontroller and makes and sends log files for ICD device 
+			sendEGMfile(&serialh, pointAStop, pointASbot, pointVStop, pointVSbot, pointSStop, pointSSbot,
+				sendAStop, sendASbot, sendVStop, sendVSbot, sendSStop, sendSSbot, buffer_data_in_X, buffer_data_in_Y,
+				buffer_data_in_Z, textfilename, &new_socket, &struct4_send, deviceid,filenum);
+
+			cout << "Finished " << textfilename << endl << endl;
+			//only wait on ICD algorithm if it is the device beign tested
+			if (deviceid == 2) {
+				cout << "Sending First Message" << endl;
+				// wait for the ICD algorithm to end, send message to tell other program that it can move on
+				send(new_socket, message, strlen(message), 0);
+				cout << "rec NI done" << endl;
+				receivesize = recv(new_socket, incoming, 1000, 0);
+				cout << "Send SEcond message" << endl;
+			}
+			send(new_socket, message, strlen(message), 0);
+
+		}
+		else if (init == "done") break;
+
+
+	}
+	
+	//close serial port
+	CloseHandle(serialh);
+	return 0;
+}
+
+
+
+
+
+
+
+
